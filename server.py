8824ad67c095318c1baf8abd5bce25c3ce92b8df
eng-abdelrahman-sharaf <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-09-05 23:27:24 +0300

    adding server functions

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
-import re
-import os
+import re, os, socket
 
 def get_file_path( file_name , ext = ".html" , dir = "./files") -> str:
     for file in os.listdir(dir):
@@ -11,3 +10,16 @@ def get_file_path( file_name , ext = ".html" , dir = "./files") -> str:
 def find_files(HTTP_request) -> list[str]:
     files = re.findall(  r"(?<=GET \/).*?(?= HTTP)", HTTP_request )
     return files
+
+def content_to_HTTP(content , found = True) -> str:
+        return f'HTTP/1.1 {"200 OK" if found else "404 NOT Found"}\n\
+        Content-Type: text/html; charset=utf-8\n\n\
+        {content}'
+
+
+def init_server( HOST, PORT):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(5)
+    print(f"Server started on port {PORT}...")
+    return server_socket
