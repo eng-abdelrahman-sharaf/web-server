5b220184784bda12ed9082bdb49724965a60e0f1
eng-abdelrahman-sharaf <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-09-06 01:09:19 +0300

    putting POST into consideration

--- server.py
+++ server.py
@@ -8,15 +8,20 @@ def get_file_path( file_name , ext = ".html" , dir = "./files") -> str:
                 return os.path.join(dir, file)
     return None
 
-def find_files(HTTP_request) -> list[str]:
-    files = re.findall(  r"(?<=GET \/).*?(?= HTTP)", HTTP_request )
-    return files
+def find_file(HTTP_request , method) -> str:
+    file = re.findall(  fr"(?<={method} \/).*?(?= HTTP)", HTTP_request )[0]
+    return file
 
 def content_to_HTTP(content , found = True) -> str:
         return f'HTTP/1.1 {"200 OK" if found else "404 NOT Found"}\r\n\
         Content-Type: text/html; charset=UTF-8\r\n\r\n\
         {content}'
 
+def HTTP_to_content(HTTP):
+    return re.findall( r"(?<=\r\n\r\n)[\s\S]*", HTTP)[0]
+
+def HTTP_type(HTTP):
+    return HTTP[:HTTP.find(" ")]
 
 def init_server( HOST, PORT):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -31,21 +36,31 @@ while True:
     client_socket, client_address = server_socket.accept()
     print(f"Connection from {client_address} has been established.")
     
-    request = client_socket.recv(1024).decode('utf-8')
+    request = client_socket.recv(32768).decode('utf-8')
 
-    files = find_files(request)
-    
-    for file in  files:
+    method = HTTP_type(request)
+    if(method == "GET"):
+        file = find_file(request , method)
         file_path = get_file_path(file)
-        print(file_path)
+        # print(file_path)
         if(file_path == None):
             HTTP_response = content_to_HTTP("<h1>404 Not Found</h1>", False)
             client_socket.sendall(HTTP_response.encode('utf-8'))
             continue
         with open(file_path, "r") as file:
             content = file.read()
-            print(content)
+            # print(content)
             HTTP_response = content_to_HTTP(content)
             client_socket.sendall(HTTP_response.encode('utf-8'))
+
+    elif(method == "POST"):
+        file = find_file(request , method)
+        content = HTTP_to_content(request)
+        file_path = os.path.join("./files", file+".html")
+        with open(file_path, "w") as file:
+            file.write(content)
+        HTTP_response = content_to_HTTP("<h1>File Created</h1>")
+
+
     client_socket.close()
     
