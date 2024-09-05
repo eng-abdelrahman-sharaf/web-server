4ed20a776a9a8f918fbeb6e89d8f6b9cb56d159f
eng-abdelrahman-sharaf <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-09-05 23:27:40 +0300

    adding the server loop

--- server.py
+++ server.py
@@ -23,3 +23,28 @@ def init_server( HOST, PORT):
     server_socket.listen(5)
     print(f"Server started on port {PORT}...")
     return server_socket
+
+server_socket = init_server("localhost", 8080)
+
+while True:
+    client_socket, client_address = server_socket.accept()
+    print(f"Connection from {client_address} has been established.")
+    
+    request = client_socket.recv(1024).decode('utf-8')
+
+    files = find_files(request)
+    
+    for file in  files:
+        file_path = get_file_path(file)
+        print(file_path)
+        if(file_path == None):
+            HTTP_response = content_to_HTTP("<h1>404 Not Found</h1>", False)
+            client_socket.sendall(HTTP_response.encode('utf-8'))
+            continue
+        with open(file_path, "r") as file:
+            content = file.read()
+            print(content)
+            HTTP_response = content_to_HTTP(content)
+            client_socket.sendall(HTTP_response.encode('utf-8'))
+    client_socket.close()
+    
